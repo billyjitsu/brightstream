a741a880d3f52479d09c90fe4390cbc7bd713f73
billyjitsu <82428978+billyjitsu@users.noreply.github.com>
2022-03-08 14:10:33 -0800

    change to use brightx

--- deploy/001_deploy_token.ts
+++ deploy/001_deploy_token.ts
@@ -7,20 +7,20 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 
   const {deployer, tokenOwner} = await getNamedAccounts();
 
-  
+ /* 
   //Rinkeby superfluid contracts
   const host = '0xeD5B5b32110c3Ded02a07c8b8e97513FAfb883B6';
   const cfa = '0xF4C5310E51F6079F601a5fb7120bC72a70b96e2A';
   const fDAIx = '0x745861AeD1EEe363b4AaA5F1994Be40b1e05Ff90';
-  
+  */
  
-  /*
+  
   //Gnosis superfluid contracts
   const host = '0x2dFe937cD98Ab92e59cF3139138f18c823a4efE7';
   const cfa = '0xEbdA4ceF883A7B12c4E669Ebc58927FBa8447C7D';
   const fDAIx = '0x59988e47A3503AaFaA0368b9deF095c818Fdca01';
   const Brightx = '0x7AecA73f38f8F33AB7FF067fED1268384d12324d';
-*/
+
 /*
   //mumbai contracts
   const host = '0xEB796bdb90fFA0f28255275e16936D25d3418603';
@@ -38,7 +38,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     "BID", 
     host,
     cfa,
-    fDAIx,
+    Brightx,
     owner],
     log: true,
   });
